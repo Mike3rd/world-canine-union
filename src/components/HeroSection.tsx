009c9c0d772fd0d6841904282e000adc5ce191f0
MikeTurko <mturko@outlook.com>
2025-12-30 16:19:43 -0500

    hero

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ export default function HeroSection() {
         {/* Hero Header */}
         <div className="text-center mb-12">
           <p className="text-2xl font-heading text-primary mb-6 bg-yellow-300 p-1 font-bold m-0">
-            Registry opens February 1, 2026
+            Site currently under construction. Registry opens February 1, 2026
           </p>
           <h1 className="text-5xl md:text-6xl font-heading font-bold text-primary mb-0">
             Soul over Pedigree
