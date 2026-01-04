73852dc19bb2358151e2be7d714d521d43e2a4d1
MikeTurko <mturko@outlook.com>
2026-01-04 18:27:48 -0500

    experimental inline

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+
+  experimental: {
+    inlineCss: true, // <-- Add this line
+  },
+
   images: {
     unoptimized: true,
     remotePatterns: [
